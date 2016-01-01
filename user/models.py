58a4199676a6b22b2245a19e01a96b0fff974c33
ducthinhdt <ducthinhdt@gmail.com>
2016-01-01 22:24:47 +0700

    Apply changes

--- user/models.py
+++ user/models.py
@@ -1,11 +1,35 @@
 from django.db import models
 from django.conf import settings
-
+from django.contrib.auth.models import (
+    AbstractUser, AbstractBaseUser
+)
 # Create your models here.
-class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
-    slug = models.SlugField(max_length=30, unique=True)
-    about = models.TextField()
+# genders = ["Male", "Female", "Other"]
 
-    def __str__(self):
-        return self.user.get_username()
+genders = (
+    (1, "Female"),
+    (2, "Male"),
+    (3, "Other"),
+)
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True
+    )
+    date_of_birth = models.DateField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = ['email']
+
+
+# class CustomUser(AbstractUser):
+#     age = models.PositiveIntegerField(null=True, blank=True)
+#     gender = models.CharField(choices=genders,
+#                               default="Female",
+#                               max_length=20, blank=True)
+#     REQUIRED_FIELDS = ['email']
+#     pass
--- user/views.py
+++ user/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.dispatch import receiver
+from allauth.account.signals import user_signed_up
 
 # Create your views here.
+@receiver(user_signed_up)
+def set_gender(sender, **kwargs):
+    user = kwargs.pop("user")
+    extra_data = user.socialaccount_set.filter(provider="facebook")[0].extra_data
+    gender = extra_data['gender']
+
+    if gender == "Male":
+        user.gender = "Male"
+    print("Inside set_gender")
+    user.save()
