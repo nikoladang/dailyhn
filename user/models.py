4c5840134cf5c4a44a36624cd34701d3de55a489
ducthinhdt <ducthinhdt@gmail.com>
2016-01-02 04:14:02 +0700

    Add custom user model

--- user/models.py
+++ user/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import (
-    AbstractUser, AbstractBaseUser
+    BaseUserManager, AbstractBaseUser, AbstractUser
 )
 # Create your models here.
 # genders = ["Male", "Female", "Other"]
@@ -12,8 +12,30 @@ genders = (
     (3, "Other"),
 )
 
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
 
-class CustomUser(AbstractBaseUser):
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth
+        )
+        user.set_password=password
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, password):
+        user = self.create_user(
+            email,
+            password=password,
+            date_of_birth=date_of_birth
+        )
+        user.is_admin=True
+        user.save(using=self._db)
+        return user
+
+class MyUser(AbstractBaseUser):
     email = models.EmailField(
         verbose_name='email address',
         max_length=255,
@@ -22,7 +44,32 @@ class CustomUser(AbstractBaseUser):
     date_of_birth = models.DateField()
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
     USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_of_birth']
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    # USERNAME_FIELD = 'email'
     # REQUIRED_FIELDS = ['email']
 
 
