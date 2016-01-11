0ee113edec51ecb4d8a5a21e55cac1c12dfc6b5b
ducthinhdt <ducthinhdt@gmail.com>
2016-01-11 18:23:43 +0700

    WhiteNoise try to fix

--- dailyhn/settings/production.py
+++ dailyhn/settings/production.py
@@ -4,11 +4,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["www.dailyhn.com","dailyhn.com","dailyhn.phanthietfood.com","www.dailyhn.phanthietfood.com"]
 
-
-STATIC_HOST = get_secret("DJANGO_STATIC_HOST")
-STATIC_URL = STATIC_HOST + '/static/'
 STATIC_ROOT = "/webapps/dailyhn_django/static/"
-
 MEDIA_ROOT = "/webapps/dailyhn_django/media/"
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
@@ -16,5 +12,9 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST =get_secret("EMAIL_HOST")
 EMAIL_PORT = get_secret("EMAIL_PORT")
 EMAIL_HOST_USER = get_secret("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = get_secret("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
+
+# WhiteNoise
+STATIC_HOST = get_secret("DJANGO_STATIC_HOST")
+STATIC_URL = STATIC_HOST + '/static/'
+WHITENOISE_ROOT = os.path.join(BASE_DIR, "../static")
