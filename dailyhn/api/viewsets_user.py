09a727a61851d6f4295f88ec0a61ffc0b360deb7
ducthinhdt <ducthinhdt@gmail.com>
2016-02-04 16:41:35 +0700

    Update changes

--- dailyhn/api/viewsets_user.py
+++ dailyhn/api/viewsets_user.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import viewsets
 from .serializers import UserSerializer
 
+
 class UserViewSet(viewsets.ModelViewSet):
     User = get_user_model()
     queryset = User.objects.all()
--- user/models.py
+++ user/models.py
@@ -103,9 +103,11 @@ class UserProfile(models.Model):
     country = CountryField()
     entries_per_day = models.IntegerField(default=10)
     days_at_homepage = models.IntegerField(default=3)
-    bookmark_icon = models.ImageField(upload_to='bookmakr_icon/%Y/%m/%d')
+    icon_symbol = models.ImageField(upload_to='icon_symbol/%Y/%m/%d', blank=True, null=True)
+    # icon_bookmark = models.ImageField(upload_to='icon_bookmark/%Y/%m/%d', width_field='32', height_field='32' ,blank=True, null=True)
+    icon_bookmark = models.ImageField(upload_to='icon_bookmark/%Y/%m/%d', blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return self.user
+        return str(self.user)
